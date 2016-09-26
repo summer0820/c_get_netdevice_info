ec5c14bef37a340c5bf5d501113e36f17dc67cf6
manfred <manfredzhaoshubo@foxmail.com>
2016-09-27 01:35:24 +0800

    get mac address

--- c_getnetdevice_info.c
+++ c_getnetdevice_info.c
@@ -15,7 +15,7 @@ int get_netdevice_info()
     struct ifreq ifr_buf[netcard_num];  //单个网卡接口信息
     struct ifreq ifr_buf_copy;           //用于保存存活网卡接口信息
     struct ifconf ifc;                  //用于取回网卡所有信息
-    char mac_addr[16];                  //mac地址
+    char mac_addr[32];                  //mac地址
     char ip_addr[32];                   //ip地址
     char broad_addr[32];                //广播地址
     char mask_addr[32];                 //子网掩码
@@ -34,7 +34,7 @@ int get_netdevice_info()
 
     ifc.ifc_len = sizeof(ifr_buf);
     ifc.ifc_buf = (__caddr_t)ifr_buf;
-    if ((ioctl(fd, SIOCGIFCONF, (char *)&ifc))<0)
+    if ((ioctl(fd, SIOCGIFCONF, (char *)(&ifc)))<0)
     {
         perror("ioctl failed");
     }
@@ -45,14 +45,38 @@ int get_netdevice_info()
 
         while(interface_num--)
         {
+            if(strcmp(ifr_buf[interface_num].ifr_name,"lo") == 0)
+            {
+                continue;
+            }
             printf("\ndevice name: %s\n",ifr_buf[interface_num].ifr_name);
 
-            ifr_buf_copy = ifr_buf[interface_num];      //将存活到接口信息保存
-            if(ioctl(fd,SIOCGIFFLAGS,(char *)&ifr_buf_copy))
+            ifr_buf_copy = ifr_buf[interface_num];      //利用ifr_buf_copy保留存活的接口
+            if(ioctl(fd,SIOCGIFFLAGS,(char *)(&ifr_buf_copy))<0)
             {
                 printf("ioctl: %s [%s:%d]\n",strerror(errno),__FILE__, __LINE__);
                 close(fd);
                 return -1;
+            }
+
+            if(ioctl(fd,SIOCGIFHWADDR,(char *)(&ifr_buf[interface_num]))<0)
+            {
+                printf("ioctl: %s [%s:%d]\n",strerror(errno),__FILE__, __LINE__);
+                close(fd);
+                return -1;
+            }
+            else
+            {
+                memset(mac_addr,0,sizeof(mac_addr));
+                snprintf(mac_addr, sizeof(mac_addr), "%02X:%02X:%02X:%02X:%02X:%02X",
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[0],
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[1],
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[2],
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[3],
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[4],
+                            (unsigned char)ifr_buf[interface_num].ifr_hwaddr.sa_data[5]);
+                printf("device %s mac adress is %s\n",ifr_buf[interface_num].ifr_name,mac_addr);
+
             }
         }
 
